a7e651befc199b6591bfaf70b65ad4d4b0e85a25
uFleury <ufleu091@uottawa.ca>
2023-11-29 17:37:49 -0500

    Add EventType spinner to FindClubActivity

--- app/src/main/java/edu/uottawa/seg2105_final_grp12/FindClubActivity.java
+++ app/src/main/java/edu/uottawa/seg2105_final_grp12/FindClubActivity.java
@@ -2,11 +2,33 @@ package edu.uottawa.seg2105_final_grp12;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import edu.uottawa.seg2105_final_grp12.models.data.EventType;
+
+
 public class FindClubActivity extends AppCompatActivity {
+
+    DatabaseReference databaseEvents;
+    DatabaseReference databaseEventTypes;
+    List<EventType> eventTypes;
+    Spinner eventTypeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +39,33 @@ public class FindClubActivity extends AppCompatActivity {
             Intent intent = new Intent(FindClubActivity.this, WelcomeActivity.class);
             startActivity(intent);
         });
+
+        EditText eventName = findViewById(R.id.eventNameField);
+
+        databaseEvents = FirebaseDatabase.getInstance().getReference("events");
+        databaseEventTypes = FirebaseDatabase.getInstance().getReference("eventTypes");
+
+        eventTypes = new ArrayList<EventType>();
+        eventTypeSpinner = findViewById(R.id.spinner_browse_event_types);
+        // just pull the existing event types once whenever the activity opens
+        databaseEventTypes.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> dataSnapshot) {
+                eventTypes.clear();
+                for (DataSnapshot postSnapshot: dataSnapshot.getResult().getChildren()) {
+                    EventType eventType = postSnapshot.getValue(EventType.class);
+                    eventTypes.add(eventType);
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(FindClubActivity.this, android.R.layout.simple_spinner_item,
+                        eventTypes.stream().map(EventType::getName).collect(Collectors.toList()));
+
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                eventTypeSpinner.setAdapter(adapter);
+            }
+        });
+
     }
+
+
 }
